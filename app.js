82ee30ba3fde6c02f5227e26e013168e8a1c03f5
sebastien <64170102+dehaut-dev@users.noreply.github.com>
2021-01-01 18:57:04 +0100

    maj
    aa

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://user-dehaut:LJbaz59MCP63ZndD@cluster0.8qplx.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({message : 'votre requete a bien ete recu'});
-})
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+
+app.use('/api/auth/', userRoutes);
+
 
 module.exports = app;
