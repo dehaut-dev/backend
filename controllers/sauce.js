4f696012484ec65162cb99d590b4faede48127bd
sebastien <64170102+dehaut-dev@users.noreply.github.com>
2021-01-14 18:38:05 +0100

    maj
    manque l'ajout du controleur pour les bouton like et dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -6,7 +6,11 @@ exports.createSauce = (req, res, next) => {
     delete sauceObject._id
     const sauce = new Sauce({
         ...sauceObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: JSON.stringify([]),
+        usersDisliked: JSON.stringify([])
     })
     sauce.save()
         .then(() => res.status(201).json({message: 'Sauce enregistré !'}))
@@ -44,11 +48,22 @@ exports.getAllSauces = (req, res, next) => {
 }
 
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({
-            _id: req.params.id
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            sauce = sauce.toObject()
+            res.status(200).json({
+                ...sauce,
+                usersLiked: JSON.parse(sauce.usersLiked),
+                usersDisliked: JSON.parse(sauce.usersDisliked)
+            })
         })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({
-            error
-        }));
+        .catch(error => res.status(400).json({error}))
+}
+
+exports.likeSauce = (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => {
+        
+    })
+    .catch(error => res.status(400).json({error}))
 }
